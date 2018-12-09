386d18f463a9b79b1627e296173ec841053392aa
apple <apple@apples-MacBook-Pro.local>
2018-12-09 19:20:04 +0000

    testing dat.gui with array

--- New Project/sketch.js
+++ New Project/sketch.js
@@ -1,15 +1,24 @@
 let getLocationL = "Berlin";
 let apiKey = "LipKCauFo7AcRa7pJ";
 let weather;
-let array = [];
+let array = '';
+var params;
 
 function preload() {
   let url = "https://api.airvisual.com/v2/countries?&key=" + apiKey;
   weather = loadJSON(url);
+
+  text = {
+	country: ''
+	}
+
 }
 
 function setup() {
-  for (var i = 0; i < weather.data.length; i++) {
-    console.log(weather.data[i].country); //Return all JSON data
-  }
+
+  var gui = new dat.GUI();
+  gui.add(text, 'country', [ weather.data[0].country ]);
+
+
+
 }
