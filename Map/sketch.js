3ba787e8318571d4e9c9052884b68430e59f3607
apple <apple@apples-MBP.connect>
2018-12-15 17:29:20 +0000

    found database and link CSV

--- Map/sketch.js
+++ Map/sketch.js
@@ -1,69 +1,29 @@
-// Themes begin
-am4core.useTheme(am4themes_animated);
-// Themes end
 
-/* Create map instance */
-var chart = am4core.create("chartdiv", am4maps.MapChart);
-
-/* Set map definition */
-chart.geodata = am4geodata_worldLow;
-
-/* Set projection */
-chart.projection = new am4maps.projections.Eckert6();
-
-/* Create map polygon series */
-var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
-
-/* Make map load polygon (like country names) data from GeoJSON */
-polygonSeries.useGeodata = true;
-
-/* Configure series */
-var polygonTemplate = polygonSeries.mapPolygons.template;
-polygonTemplate.applyOnClones = true;
-polygonTemplate.togglable = true;
-polygonTemplate.tooltipText = "{name}";
-polygonTemplate.strokeWidth = 0.5;
-polygonTemplate.strokeOpacity = 0.5;
-polygonTemplate.fill = chart.colors.getIndex(0);
-var lastSelected;
-polygonTemplate.events.on("hit", function(ev) {
-  if (lastSelected) {
-    // This line serves multiple purposes:
-    // 1. Clicking a country twice actually de-activates, the line below
-    //    de-activates it in advance, so the toggle then re-activates, making it
-    //    appear as if it was never de-activated to begin with.
-    // 2. Previously activated countries should be de-activated.
-    lastSelected.isActive = false;
-  }
-  ev.target.series.chart.zoomToMapObject(ev.target);
-  if (lastSelected !== ev.target) {
-    lastSelected = ev.target;
-  }
-})
+var csv;
+let apiKeyM = "pk.eyJ1IjoiYXJ0aHVydmVycmVwdCIsImEiOiJjanBqdWFuc2EwYTFxM3ZwZjNlcnBvN2ZtIn0.AP8VWsR0JqBf1JItg_VMvw";
 
 
-/* Create selected and hover states and set alternative fill color */
-var ss = polygonTemplate.states.create("active");
-ss.properties.fill = chart.colors.getIndex(5).brighten(-0.5);
+var year = '2004-';
+var month = '12-';
 
-var hs = polygonTemplate.states.create("hover");
-hs.properties.fill = chart.colors.getIndex(0).brighten(-0.5);
+function setup(){
+  csv = loadJSON("DisasterData.json");
+  console.log(csv);
 
-// Hide Antarctica
-polygonSeries.exclude = ["AQ"]
+}
 
-// Zoom control
-chart.zoomControl = new am4maps.ZoomControl();
-
-var homeButton = new am4core.Button();
-homeButton.events.on("hit", function(){
-  chart.goHome();
+mapboxgl.accessToken = apiKeyM;
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/basic-v9'
 });
 
-homeButton.icon = new am4core.Sprite();
-homeButton.padding(7, 5, 7, 5);
-homeButton.width = 20;
-homeButton.icon.path = "M16,8 L14,8 L14,16 L10,16 L10,10 L6,10 L6,16 L2,16 L2,8 L0,8 L8,0 L16,8 Z M16,8";
-homeButton.marginBottom = 10;
-homeButton.parent = chart.zoomControl;
-homeButton.insertBefore(chart.zoomControl.plusButton);
+
+   map.on('mousemove', function (e) {
+        // e.point is the x, y coordinates of the mousemove event relative
+        // to the top-left corner of the map
+        //JSON.stringify(e.point)
+        // e.lngLat is the longitude, latitude geographical position of the event
+        console.log(JSON.stringify(e.lngLat));
+
+      });
