dbc0d8e63aa46bfa673cd521bf4e1ba7f04b16f6
apple <apple@apples-MBP.fritz.box>
2018-12-20 14:42:07 +0000

    updated code using promises to clean up and to handle errors in future

--- Map/sketch.js
+++ Map/sketch.js
@@ -10,37 +10,53 @@ var urlD = 'https://api.reliefweb.int/v1/disasters?appname=arthur-verrept@hotmai
 
 
 function preload(){
-loadJSON(urlD, loadD)
+  //loading disaster data with callback to anonymous function to load data into an array
+  //use of arrow syntax to remove function word. previously: function(disaster){...}
+  fetch(urlD).then(function(response) {
+    return response.json();
+  })
+  .then(function(disaster){
+    for (var i = 0; i < disaster.data.length; i++){
+      disasterIso[i] = 'http://countryapi.gear.host/v1/Country/getCountries?pAlpha3Code=' + disaster.data[i].fields.country[0].iso3  
+    }
+    console.log(disasterIso);
+});
+
+
+
+  // loadJSON(urlD, (disaster) => {
+  //   for (var i = 0; i < disaster.data.length; i++){
+  //     disasterIso[i] = 'http://countryapi.gear.host/v1/Country/getCountries?pAlpha3Code=' + disaster.data[i].fields.country[0].iso3
+  //   }
+  //   loadCoords();
+  // });
 }
 
-function loadD(disaster){
-  for (var i = 0; i < disaster.data.length; i++){
-    disasterIso[i] = 'http://countryapi.gear.host/v1/Country/getCountries?pAlpha3Code=' + disaster.data[i].fields.country[0].iso3
-  }
-  loadCoords();
-}
 
 function loadCoords(){
   for (var i = 0; i < disasterIso.length; i++) {
     disasterLoc[i] = loadJSON(disasterIso[i])
   }
+  console.log(disasterLoc);
 }
 
 function setup(){
- mapboxgl.accessToken = apiKeyM;
+}
 
-let map = new mapboxgl.Map({
-container: 'app',
-style: 'mapbox://styles/mapbox/basic-v9'
-});
-
+function loadMap(){
+  mapboxgl.accessToken = apiKeyM;
+ let map = new mapboxgl.Map({
+ container: 'app',
+ style: 'mapbox://styles/mapbox/basic-v9'
+ });
+ map.on('load', function() {
   map.addSource('pointSource',{
   type: 'geojson',
   data:   {
     "type": "Feature",
     "geometry": {
       "type": "Point",
-      "coordinates": [0, 0]
+      "coordinates": [disasterLoc[0].response[0].latitude, disasterLoc[0].response[0].longitude]
     },
     "properties": {
       "name": "Dinagat Islands"
@@ -52,4 +68,5 @@ style: 'mapbox://styles/mapbox/basic-v9'
     source:'pointSource',
     type: 'circle'
 });
+})
 }
