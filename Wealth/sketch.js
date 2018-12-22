ee285755faf787f19eb83878a10314426250d91b
apple <apple@apples-MBP.fritz.box>
2018-12-22 18:46:11 +0100

    Mapping interactions and text to values from array

--- Wealth/sketch.js
+++ Wealth/sketch.js
@@ -1,5 +1,7 @@
 var url = 'https://forbes400.herokuapp.com/api/forbes400?limit=100';
 var data = [];
+var upper;
+
 
 function preload(){
   createCanvas(1280, 720)
@@ -8,21 +10,58 @@ function preload(){
 
 function setup() {
   console.log(data)
-  noLoop();
-  textAlign(CENTER)
+  upper = createSlider(0, 100, 10);
 }
 
 function draw() {
-  var x = 0;
-  var y = 0;
+  background(240);
+  var r = upper.value();
+  console.log(r)
   fill(255, 0, 0);
-  textSize(10);
-  for (var i = 0; i < 10; i++) {
-    text(data[i].name, (width - 30) * (i / 10) + 15 + 30, 20)
-    text('$' + financial(data[i].realTimeWorth * 1000000/1000000000) + ' B', (width - 30) * (i / 10) + 15 + 30, 40)
+  let total = 0;
+  for (var i = 0; i < r; i++) {
+    textSize(10);
+    console.log(data[i].name);
+    text(data[i].name, (width) * (i / r)+10, 20)
+    text('$' + financial(data[i].realTimeWorth * 1000000/1000000000) + ' B', (width) * (i / r)+10, 40)
   }
+  for (var i = 0; i < r; i++) {
+    if (data[i].realTimeWorth != null) {
+      var num = Math.round(data[i].realTimeWorth/1000);
+      total += num;
+    }
+    else{
+      i++;
+    }
+  }
+  textAlign(CENTER)
+  if (total > 1000) {
+    textSize(map(total, 1000, 2000, 15, 25))
+    text('$' + precise(total/1000) + ' TRILLION', width/2, height/2)
+  }
+  else{
+    textSize(map(total, 0, 1000, 7.5, 15))
+    text('$' + precise(total) + ' BILLION', width/2, height/2)
+  }
+
+}
+
+function precise(x) {
+  return Number.parseFloat(x).toPrecision(4);
 }
 
 function financial(x) {
 return Number.parseFloat(x).toFixed(2);
+}
+
+function checkData(){
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].realTimeWorth == null) {
+      data.splice(i,1)
+    }
+    else{
+      i++;
+    }
+  }
+
 }
