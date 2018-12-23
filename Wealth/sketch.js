791d595a3db2cecefc3bd903b1c0b7d11e78ed40
apple <apple@apples-MBP.fritz.box>
2018-12-23 13:01:38 +0100

    creating objects to neaten code

--- Wealth/sketch.js
+++ Wealth/sketch.js
@@ -5,6 +5,17 @@ var temp =[];
 var upper;
 var img = [];
 
+var words = {
+  x: 0,
+  y: 20,
+  size: 0
+};
+
+var pic = {
+  x: 0,
+  y: 50
+}
+
 function preload(){
   createCanvas(1280, 720)
   data = loadJSON(url)
@@ -22,24 +33,38 @@ function setup() {
 
 
 function draw() {
+richList();
+}
+
+function precise(x) {
+  return Number.parseFloat(x).toPrecision(4);
+}
+
+function financial(x) {
+return Number.parseFloat(x).toFixed(2);
+}
+
+function richList(){
   background(240);
   var textHeight = 20;
   var r = upper.value();
   fill(255, 0, 0);
   let total = 0;
-
   for (var i = 0; i < r; i++) {
+    words.x = (width - 10) * (i / r)+60;
+    pic.x = (width - 10) * (i / r)+60;
     if (r <= 20) {
       if (data[i].name.length < 11) {
-        image(img[i], (width - 20) * (i / r)+40, textHeight, 50, 50);
+        image(img[i], pic.x, pic.y, 100, 100);
         textSize(map(r, 0, 20, 20, 7.5));
-        text(data[i].name, (width - 20) * (i / r)+40, textHeight)
-        text('$' + financial(data[i].realTimeWorth * 1000000/1000000000) + ' B', (width - 20) * (i / r)+40, textHeight*2 - map(r, 0, 25, 0,12));
+        text(data[i].name, words.x, words.y)
+        text('$' + financial(data[i].realTimeWorth * 1000000/1000000000) + ' B', words.x, words.y*2 - map(r, 0, 25, 0,12));
       }
       else{
+        image(img[i], (width - 10) * (i / r)+60, textHeight*2+10, 100, 100);
         textSize(map(r, 0, 20, 20, 7.5));
-        text(data[i].lastName, (width - 20) * (i / r)+40, textHeight)
-        text('$' + financial(data[i].realTimeWorth * 1000000/1000000000) + ' B', (width - 20) * (i / r)+40, textHeight*2 - map(r, 0, 25, 0,12));
+        text(data[i].lastName, (width - 10) * (i / r)+60, textHeight)
+        text('$' + financial(data[i].realTimeWorth * 1000000/1000000000) + ' B', (width - 10) * (i / r)+60, textHeight*2 - map(r, 0, 25, 0,12));
       }
     }
   }
@@ -65,12 +90,4 @@ function draw() {
     text('$' + precise(total) + ' BILLION', width/2, height/2)
   }
 
-}
-
-function precise(x) {
-  return Number.parseFloat(x).toPrecision(4);
-}
-
-function financial(x) {
-return Number.parseFloat(x).toFixed(2);
 }
