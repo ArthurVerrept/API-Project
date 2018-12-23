5304a4dfe80b7888a570823f64b28fc094fff371
apple <apple@apples-MBP.fritz.box>
2018-12-23 19:14:37 +0100

    added changes to format and started list

--- Wealth/sketch.js
+++ Wealth/sketch.js
@@ -5,6 +5,7 @@ var temp =[];
 var upper;
 var img = [];
 var current;
+var flags = [];
 
 var words = {
   x: 0,
@@ -32,8 +33,11 @@ function setup() {
   rectMode(CENTER);
   noStroke();
 
-  for (var i = 0; i < 30; i++) {
+  for (var i = 0; i < 20; i++) {
     img[i] = loadImage("images/" + data[i].name + ".jpg");
+  }
+  for (var i = 0; i < 9; i++) {
+    flags[i] = loadImage("Flags/" + data[i].country + ".png");
   }
 }
 
@@ -57,51 +61,53 @@ function richList(){
   var int = 0;
   fill(240);
   for (var i = 0; i < r; i++) {
+      console.log(i + data[i].industry)
       words.x = (width-80) * (i / r)+80;
       pic.x = (width-80) * (i / r)+80;
       pic.size = map(r, 0, 20, 90, 60)
         if (checkMouse(pic.x, pic.y, pic.size) == true) {
-          background(240)
-          fill(240,240,240, 100)
+          background(240);
+          fill(240,240,240, 150)
           tint(240, 126);
           for (var x = 0; x < i; x++) {
             words.x = (width-80) * (x / r)+80;
             pic.x = (width-80) * (x / r)+80;
             pic.size = map(r, 0, 20, 90, 60)
             image(img[x], pic.x, pic.y, pic.size*0.7, pic.size*0.7);
-            textSize(map(r, 0, 20, 18, 6.75)*0.7);
-            text(data[x].name, words.x, words.y)
-            text('$' + financial(data[x].realTimeWorth * 1000000/1000000000) + ' B', words.x, words.y+18 - map(r, 0, 25, 0,12));
+            textSize(map(r, 0, 20, 50, 30)*0.7);
+            text('#' + data[x].rank, words.x, words.y+10)
+            //text('$' + financial(data[x].realTimeWorth * 1000000/1000000000) + ' B', words.x, words.y+18 - map(r, 0, 25, 0,12));
           }
           for (var x = i+1; x < r; x++) {
             words.x = (width-80) * (x / r)+80;
             pic.x = (width-80) * (x / r)+80;
             pic.size = map(r, 0, 20, 90, 60)
             image(img[x], pic.x, pic.y, pic.size*0.7, pic.size*0.7);
-            textSize(map(r, 0, 20, 18, 6.75)*0.7);
-            text(data[x].name, words.x, words.y)
-            text('$' + financial(data[x].realTimeWorth * 1000000/1000000000) + ' B', words.x, words.y+18 - map(r, 0, 25, 0,12));
+            textSize(map(r, 0, 20, 50, 30)*0.7);
+            text('#' + data[x].rank, words.x, words.y+10)
+            //text('$' + financial(data[x].realTimeWorth * 1000000/1000000000) + ' B', words.x, words.y+18 - map(r, 0, 25, 0,12));
           }
           selected();
         }
         else{
           image(img[i], pic.x, pic.y, pic.size, pic.size);
-          textSize(map(r, 0, 20, 18, 6.75));
-          text(data[i].name, words.x, words.y)
-          text('$' + financial(data[i].realTimeWorth * 1000000/1000000000) + ' B', words.x, words.y+18 - map(r, 0, 25, 0,12));
+          textSize(map(r, 0, 20, 50, 30));
+          text('#' + data[i].rank, words.x, words.y+10)
+          //text('$' + financial(data[i].realTimeWorth * 1000000/1000000000) + ' B', words.x, words.y+18 - map(r, 0, 25, 0,12));
         }
     }
 
 function selected(){
-  fill(240)
+  textAlign(LEFT);
+  fill(20)
   tint(240, 255);
-  words.x = (width-130) * (i / r)+130;
-  pic.x = (width-130) * (i / r)+130;
-  pic.size = map(r, 0, 20, 90, 60)
-  image(img[i], pic.x, pic.y+200, pic.size*4, pic.size*4);
+  pic.x = (width-360) * (i / r)+200;
+  pic.size = map(r, 0, 20, 90, 60)*4
+  words.x = pic.x + pic.size-map(r, 0, 20, 150, 100);
+  image(img[i], pic.x, pic.y+200, pic.size, pic.size);
   textSize(map(r, 0, 20, 18, 6.75)*2);
   text(data[i].name, words.x, words.y+200)
-  text('$' + financial(data[i].realTimeWorth * 1000000/1000000000) + ' B', words.x, words.y+240 - map(r, 0, 25, 0,12));
+  text('Net Worth: $' + financial(data[i].realTimeWorth * 1000000/1000000000) + ' BILLION', words.x, words.y+240 - map(r, 0, 25, 0,12));
   i = x;
 }
 
@@ -118,11 +124,11 @@ function selected(){
   fill(20);
   textAlign(CENTER)
   if (total > 1000) {
-    textSize(map(total, 1000, 2000, 15, 25))
+    textSize(map(total, 1000, 2000, 100, 300))
     text('$' + precise(total/1000) + ' TRILLION', width/2, height/1.5)
   }
   else{
-    textSize(map(total, 0, 1000, 7.5, 15))
+    textSize(map(total, 0, 1000, 50, 100))
     text('$' + precise(total) + ' BILLION', width/2, height/1.5)
   }
 }
